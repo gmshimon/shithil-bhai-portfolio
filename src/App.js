96b7a3bbfd2483149727ad16f8e9cd7346d4573d
GMShimon <simon.rosedale99@gmail.com>
2023-03-27 01:07:31 +0800

    navbar done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <Header></Header>
     </div>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,28 @@
-import React from 'react';
-
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import './Header.css'
 const Header = () => {
-    return (
-        <div>
-            <p>Hello</p>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand className="me-3 " href="#home">Sheikh Shithil</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className="text-white me-3 links">Publications</Nav.Link>
+            <Nav.Link href="#features" className="text-white me-3 links">Featured Works</Nav.Link>
+            <Nav.Link href="#pricing" className="text-white me-3 links">Projects</Nav.Link>
+            <Nav.Link href="#pricing" className="text-white me-3 links">News</Nav.Link>
+            <Nav.Link href="#pricing" className="text-white me-3 links">Curriculum vitae</Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
