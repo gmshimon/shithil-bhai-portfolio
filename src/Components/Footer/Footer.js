a505fa6d2b9e6e97eef43aa10c64880472698ed7
gmshimon <simon.rosedale99@gmail.com>
2023-07-13 09:30:56 +0800

    [DONE] footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,8 +8,15 @@ const Footer = () => {
     window.scrollTo({ top: 0, behavior: 'auto' })
   }
   return (
-    <div>
+    <div className='d-flex justify-content-center' style={{margin:'90px 0 70px 0'}}>
+      <div>
+        <p style={{fontSize:'1.1rem'}}>
+        Powered by the <a target='_blank' href="https://www.sourcethemes.com/academic/">Academic theme</a> for <a target='_blank' href="https://gohugo.io/">Hugo</a>.
+        </p>
+      </div>
+      <div style={{marginLeft:'250px'}}>
       <FaArrowUp className="up-arrow-icon"  onClick={scrollToTop}/>
+      </div>
     </div>
   )
 }
